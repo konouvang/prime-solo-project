27de5a4d6d08bf7cf4fe496c1394d8663a0c20f1
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-30 13:13:10 -0500

    secret router is updated

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -4,12 +4,20 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+    if(req.isAuthenticated()){
+        const queryString =`SELECT * FROM "secret" WHERE "secrecy_level" <= $1;`;
+
+        pool.query(queryString, [req.user.clearance_level] )
+            .then(results => res.send(results.rows))
+            .catch(error => {
+                console.log('Error making SELECT for secrets:', error);
+                res.sendStatus(500);
+            });
+    }else{
+        ((err) => {
+            res.sendStatus(403);
+        })
+    }
 });
 
 module.exports = router;
