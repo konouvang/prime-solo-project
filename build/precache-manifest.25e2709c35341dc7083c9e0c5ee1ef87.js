e12c3efc0d1d6dafb3c7b25e463528aa2708e7b2
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-30 13:39:20 -0500

    done

---
+++ build/precache-manifest.25e2709c35341dc7083c9e0c5ee1ef87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fd09b3e5a8435be3762d82901049c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ecfe9a223f99ecc70cf",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "a67722879ae0ebc74b27",
+    "url": "/static/js/2.8bc4b2c9.chunk.js"
+  },
+  {
+    "revision": "5ecfe9a223f99ecc70cf",
+    "url": "/static/js/main.180371b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
